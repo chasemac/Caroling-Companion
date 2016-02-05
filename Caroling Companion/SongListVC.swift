6e0c952ae2893bbe3b882cf4b620527daea442a6
Chase McElroy <ichasem@gmail.com>
2016-02-04 19:15:17 -0800

    Added all song lyrics to songs.swift

--- Caroling Companion/SongListVC.swift
+++ Caroling Companion/SongListVC.swift
@@ -37,82 +37,82 @@ class SongListVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         
         let FrostyTheSnowman = Song()
         FrostyTheSnowman.name = "Frosty The Snowman"
-        FrostyTheSnowman.lyrics = "FrostyTheSnowman"
+        FrostyTheSnowman.lyrics = frostyTheSnowman
         self.songs.append(FrostyTheSnowman)
         
         let HarkTheHarald = Song()
         HarkTheHarald.name = "Hark The Harald"
-        HarkTheHarald.lyrics = "HarkTheHarald"
+        HarkTheHarald.lyrics = harkTheHarold
         self.songs.append(HarkTheHarald)
         
         let HaveYourselfAMerryLittleChristmas = Song()
         HaveYourselfAMerryLittleChristmas.name = "Have Yourself A Merry Little Christmas"
-        HaveYourselfAMerryLittleChristmas.lyrics = "HaveYourselfAMerryLittleChristmas"
+        HaveYourselfAMerryLittleChristmas.lyrics = haveYourselfAMerryLittleChristmas
         self.songs.append(HaveYourselfAMerryLittleChristmas)
         
         let HereComesSantaClaus = Song()
         HereComesSantaClaus.name = "Here Comes Santa Claus"
-        HereComesSantaClaus.lyrics = "HereComesSantaClaus"
+        HereComesSantaClaus.lyrics = hereComesSantaClause
         self.songs.append(HereComesSantaClaus)
         
         let ItCameUponAMidnightClear = Song()
         ItCameUponAMidnightClear.name = "It Came Upon A Midnight Clear"
-        ItCameUponAMidnightClear.lyrics = "ItCameUponAMidnightClear"
+        ItCameUponAMidnightClear.lyrics = itCameUponAMidnightClear
         self.songs.append(ItCameUponAMidnightClear)
         
         let JingleBells = Song()
         JingleBells.name = "Jingle Bells"
-        JingleBells.lyrics = "JingleBells"
+        JingleBells.lyrics = jingleBells
         self.songs.append(JingleBells)
         
         let JoyToTheWorld = Song()
         JoyToTheWorld.name = "Joy To The World"
-        JoyToTheWorld.lyrics = "JoyToTheWorld"
+        JoyToTheWorld.lyrics = joyToTheWorld
         self.songs.append(JoyToTheWorld)
         
         let LetItSnow = Song()
         LetItSnow.name = "Let It Snow"
-        LetItSnow.lyrics = "LetItSnow"
+        LetItSnow.lyrics = letItSnow
         self.songs.append(LetItSnow)
         
         let LittleDrummer = Song()
         LittleDrummer.name = "Little Drummer"
-        LittleDrummer.lyrics = "LittleDrummer"
+        LittleDrummer.lyrics = littleDrummerBoy
         self.songs.append(LittleDrummer)
         
         let OChristmasTree = Song()
         OChristmasTree.name = "O Christmas Tree"
-        OChristmasTree.lyrics = "OChristmasTree"
+        OChristmasTree.lyrics = oCrhsistmasTree
         self.songs.append(OChristmasTree)
         
         let OComeAllYeFaithful = Song()
         OComeAllYeFaithful.name = "O Come All Ye Faithful"
-        OComeAllYeFaithful.lyrics = "OComeAllYeFaithful"
+        OComeAllYeFaithful.lyrics = oComeAllYeFaithful
         self.songs.append(OComeAllYeFaithful)
         
         let RudolphTheRedNoseReindeer = Song()
         RudolphTheRedNoseReindeer.name = "Rudolph The Red Nose Reindeer"
-        RudolphTheRedNoseReindeer.lyrics = "RudolphTheRedNoseReindeer"
+        RudolphTheRedNoseReindeer.lyrics = rudolphTheRedNoseReindeer
         self.songs.append(RudolphTheRedNoseReindeer)
         
         let SilentNight = Song()
         SilentNight.name = "Silent Night"
-        SilentNight.lyrics = "SilentNight"
+        SilentNight.lyrics = silentNight
         self.songs.append(SilentNight)
         
         let SilverBells = Song()
         SilverBells.name = "Silver Bells"
-        SilverBells.lyrics = "SilverBells"
+        SilverBells.lyrics = silverBells
         self.songs.append(SilverBells)
         
         let TheFirstNoel = Song()
         TheFirstNoel.name = "The First Noel"
-        TheFirstNoel.lyrics = "TheFirstNoel"
+        TheFirstNoel.lyrics = theFirstNoel
         self.songs.append(TheFirstNoel)
         
         let WinterWonderland = Song()
         WinterWonderland.name = "Winter Wonder land"
-        WinterWonderland.lyrics = "WinterWonderland"
+        WinterWonderland.lyrics = winterWonderland
         self.songs.append(WinterWonderland)
 
     }
