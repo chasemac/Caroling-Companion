1bdbb05250145adf953a9eddd1ecac51815281e6
Chase McElroy <ichasem@gmail.com>
2016-06-30 10:33:19 -0700

    Cleaned up code

--- Caroling Companion/SongLyricsVC.swift
+++ Caroling Companion/SongLyricsVC.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Chase McElroy on 12/20/15.
 //  Copyright © 2015 Chase McElroy. All rights reserved.
-//
 
 import UIKit
 import Firebase
@@ -15,10 +14,7 @@ class SongLyricsVC: UIViewController {
     
     @IBOutlet weak var txtView: UITextView!
     @IBOutlet weak var songTitle: UILabel!
-
-
     @IBOutlet weak var videoView: UIWebView!
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,28 +29,22 @@ class SongLyricsVC: UIViewController {
         txtView.text = lyricsSwift
         songTitle.text = title
 
-        
         let youtubeURL = YOUTUBE_URL + video
         
         videoView.allowsInlineMediaPlayback = true
         
         videoView.loadHTMLString("<iframe width=\"\(videoView.frame.width)\" height=\"\(videoView.frame.height)\" src=\"\(youtubeURL)?&playsinline=1\" frameborder=\"0\" allowfullscreen></iframe>", baseURL: nil)
-        
-        
-
     }
-    
 
-    
     func initText() {
         txtView.font = UIFont.preferredFontForTextStyle(UIFontTextStyleBody)
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(SongLyricsVC.preferredContentSizeChanged(_:)), name: UIContentSizeCategoryDidChangeNotification, object: nil)
         
         // this bug still exists in iOS 9 --- I don't know if it's still a bug but there's a bug preventing all of this from working right now....
-//        txtView.scrollEnabled = false
-//        txtView.scrollEnabled = true
-//        txtView.scrollRectToVisible(CGRectMake(0, 0, 1, 1), animated: false)
+        txtView.scrollEnabled = false
+        txtView.scrollEnabled = true
+        txtView.scrollRectToVisible(CGRectMake(0, 0, 1, 1), animated: false)
     }
     
     func preferredContentSizeChanged(notification: NSNotification) {
