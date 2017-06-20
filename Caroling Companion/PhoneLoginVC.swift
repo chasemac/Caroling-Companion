d6631526ada0e9cf32cfac276cd6caa4bd368cee
Chase McElroy <ichasem@gmail.com>
2017-06-19 17:46:38 -0700

    created phone number formatter

--- Caroling Companion/PhoneLoginVC.swift
+++ Caroling Companion/PhoneLoginVC.swift
@@ -12,11 +12,15 @@ class PhoneLoginVC: LoginFlow {
     var signup: Bool = true
 
     @IBOutlet weak var phoneNumberField: UITextField!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    @IBAction func numberPressed(_ sender: UITextField) {
+        let text = sender.text!
+        phoneNumberField.text = format(phoneNumber: text) ?? text
     }
     @IBAction func nextBtnPressed(_ sender: Any) {
         performSegue(withIdentifier: "PhoneLoginCodeVC", sender: nil)
@@ -25,5 +29,71 @@ class PhoneLoginVC: LoginFlow {
     @IBAction func backBtnPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+    
+    
 
+}
+
+func format(phoneNumber sourcePhoneNumber: String) -> String? {
+    
+    // Remove any character that is not a number
+    let numbersOnly = sourcePhoneNumber.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
+    let length = numbersOnly.characters.count
+    let hasLeadingOne = numbersOnly.hasPrefix("1")
+    
+    // Check for supported phone number length
+    guard length == 7 || length == 10 || (length == 11 && hasLeadingOne) else {
+        return nil
+    }
+    
+    let hasAreaCode = (length >= 10)
+    var sourceIndex = 0
+    
+    // Leading 1
+    var leadingOne = ""
+    if hasLeadingOne {
+        leadingOne = "1 "
+        sourceIndex += 1
+    }
+    
+    // Area code
+    var areaCode = ""
+    if hasAreaCode {
+        let areaCodeLength = 3
+        guard let areaCodeSubstring = numbersOnly.characters.substring(start: sourceIndex, offsetBy: areaCodeLength) else {
+            return nil
+        }
+        areaCode = String(format: "(%@) ", areaCodeSubstring)
+        sourceIndex += areaCodeLength
+    }
+    
+    // Prefix, 3 characters
+    let prefixLength = 3
+    guard let prefix = numbersOnly.characters.substring(start: sourceIndex, offsetBy: prefixLength) else {
+        return nil
+    }
+    sourceIndex += prefixLength
+    
+    // Suffix, 4 characters
+    let suffixLength = 4
+    guard let suffix = numbersOnly.characters.substring(start: sourceIndex, offsetBy: suffixLength) else {
+        return nil
+    }
+    
+    return leadingOne + areaCode + prefix + "-" + suffix
+}
+
+extension String.CharacterView {
+    /// This method makes it easier extract a substring by character index where a character is viewed as a human-readable character (grapheme cluster).
+    internal func substring(start: Int, offsetBy: Int) -> String? {
+        guard let substringStartIndex = self.index(startIndex, offsetBy: start, limitedBy: endIndex) else {
+            return nil
+        }
+        
+        guard let substringEndIndex = self.index(startIndex, offsetBy: start + offsetBy, limitedBy: endIndex) else {
+            return nil
+        }
+        
+        return String(self[substringStartIndex ..< substringEndIndex])
+    }
 }
